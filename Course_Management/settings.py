25b11f0d04aecb616a33567dfab1566b01208511
Prashanth <guntaprashanthkumar@gmail.com>
2024-08-05 12:51:24 +0530

    'version 2024.08.05'

--- Course_Management/settings.py
+++ Course_Management/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Course_Management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': 'Kumar@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
